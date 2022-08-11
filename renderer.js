b583f86b9b654d1a8965e82a56c1b1822074b4af
LordFB <67217617+LordFB@users.noreply.github.com>
2022-08-11 08:36:37 +0200

    updates

--- renderer.js
+++ renderer.js
@@ -32,6 +32,12 @@ export class WorldRenderer {
         this.skyUniforms = {
             time: {
                 value: 0.0
+            },
+            realtime: {
+                value: 0
+            },
+            resolution: {
+                value: new THREE.Vector2(window.innerWidth, window.innerHeight)
             }
         }
         var material = new THREE.ShaderMaterial({
@@ -44,31 +50,62 @@ export class WorldRenderer {
             }`,
             fragmentShader: `varying vec2 vUv;
             uniform float time;
+            uniform float realtime;
+            uniform vec2 resolution;
             float random(vec2 ab) 
             {
                 float f = (cos(dot(ab ,vec2(21.9898,78.233))) * 43758.5453);
                 return fract(f);
             }
+            vec4 blend(vec4 source, vec4 dest, float alpha)
+                {
+                    return source * alpha + dest * (1.0-alpha);
+                }
+
+            float dist( vec2 a, vec2 b){
+                return 0.0;
+            }
             void main() {
                 float t = 1.0 - time;
+                vec3 color;
+                vec3 disc;
+                vec4 sunColor;
+                
                 if ( vUv.y > 0.4 ){
+                    float a = 0.0;
+                    vec2 factor = vec2(1.6,0.9);
+                    vec2 sunPos = vec2(-0.8 + realtime / 520.0,time);
+                    a = distance(vUv * factor,sunPos) * 2.0;
+                   
                     float rand = random( vUv ) * 0.05;
                     vec3 skyDayTop = vec3(0.0,0.2,1.0 - rand);
                     vec3 skyDayBottom = vec3(0.7,0.8 - rand,1.0 - rand);
-                    vec3 skyNightTop = vec3(0.0,0.01,0.1 + rand * 0.5);
+                    vec3 skyNightTop = vec3(0.1 + time,time * 0.75,0);
                     vec3 skyNightBottom = vec3(0.0,0.0,0.0);
+                    sunColor = vec4(time + 0.1,time * 0.8,0.1,a + random(vUv));
+
                     vec3 skyDay = mix(skyDayTop,skyDayBottom,vUv.y - 0.1);
                     vec3 skyNight = mix(skyNightTop,skyNightBottom,vUv.y - 0.1);
                     vec3 sky = mix(skyDay, skyNight, t);
-                    gl_FragColor = vec4(sky, 1.0);
+
+                    vec4 sunAlpha;
+                    
+                    if ( distance(vUv * factor,sunPos) < 0.1 ){
+                        disc = vec3(0.8,0.8,0.1);
+                    }
+                    else{
+                        color = vec3(sky);
+                    }
                 }
                 else{
                     float rand = random( vUv ) * 0.05;
                     vec3 dark = vec3(0.1,0.15 - rand,0.1);
                     vec3 light = vec3(0.3,0.45 - rand,0.3);
                     vec3 grass = mix(dark,light,time);
-                    gl_FragColor = vec4(grass, 1.0);
+                    color = vec3(grass);
                 }
+
+                gl_FragColor = vec4(mix(disc * 10.0,color,0.9),1.0);
             }`
 
         });
@@ -84,6 +121,7 @@ export class WorldRenderer {
         //console.log( p.hour, p.minute)
         let b = this.brightnessLookup(p.hour, p.minute);
         this.backgroundPlane.material.uniforms.time.value = b;
+        this.backgroundPlane.material.uniforms.realtime.value = p.hour * 60 + p.minute;
         for (const a in window.Goap) {
             const agent = window.Goap[a]
             if (agent.parameters.position !== undefined && agent.parameters.rendererSpawned === undefined) {
@@ -111,26 +149,26 @@ export class WorldRenderer {
             0.0,
             0.0,
             0.0,
-            0.05,
+            0.0,
             0.1,
             0.2,
             0.3, // 0600
             0.4,
+            0.5,
             0.6,
+            0.7,
             0.8,
+            0.9, // 1200
+            1.0,
+            1.0,
             0.9,
-            1.0,
-            1.0, // 1200
-            1.0,
-            1.0,
-            1.0,
-            1.0,
-            1.0,
-            1.0, // 1600 
             0.8,
-            0.6,
+            0.7,
+            0.6, // 1600 
+            0.5,
             0.4,
-            0.25,
+            0.3,
+            0.2,
             0.1,
             0.0
         ];
